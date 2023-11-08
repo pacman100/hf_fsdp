d51ec6a7b3392be4d335acd312a11ce3b0ed201b
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-11-08 22:15:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ from tqdm import tqdm
 from transformers import LlamaForCausalLM
 from llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
-# replace_llama_attn_with_flash_attn()
+replace_llama_attn_with_flash_attn()
 
 def main():
 
